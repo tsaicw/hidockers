b52b339dd54446722940a95d3b344b6f89494e2d
Daniel Tsai <galoistsai@gmail.com>
2024-08-25 22:02:27 +0800

    [EMQX] Script to initialize authentication and authorization
    Signed-off-by: Daniel Tsai <galoistsai@gmail.com>

---
+++ iot/emqx/scripts/init_acl.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+API_KEY=
+SECRET_KEY=
+EMQX_DASHBOARD_BASE_URL=http://localhost:18083
+
+# Create authenticator
+curl -X POST -u ${API_KEY}:${SECRET_KEY} \
+     -H 'Content-Type: application/json' \
+     ${EMQX_DASHBOARD_BASE_URL}/api/v5/authentication \
+     --data '{
+               "mechanism":"password_based",
+               "backend":"built_in_database",
+               "password_hash_algorithm":{
+                 "name":"sha256",
+                 "salt_position":"suffix"
+               },
+               "user_id_type":"username"
+             }'
+
+# Import users into authenticator
+curl -X POST -u ${API_KEY}:${SECRET_KEY} \
+     -H 'Content-Type: application/json' \
+     ${EMQX_DASHBOARD_BASE_URL}/api/v5/authentication/password_based:built_in_database/import_users?type=plain \
+     --data '[
+               {
+                 "is_superuser": true,
+                 "password": "admin",
+                 "user_id": "admin"
+               },
+               {
+                 "is_superuser": false,
+                 "password": "user1",
+                 "user_id": "user1"
+               },
+               {
+                 "is_superuser": false,
+                 "password": "user2",
+                 "user_id": "user2"
+               }
+             ]'
+
+# Create a new authorization source
+curl -X POST -u ${API_KEY}:${SECRET_KEY} \
+     -H 'Content-Type: application/json' \
+     ${EMQX_DASHBOARD_BASE_URL}/api/v5/authorization/sources \
+     --data '{
+               "type": "built_in_database",
+               "enable": true
+             }'
+
+# Create the list of rules for All Users
+curl -X POST -u ${API_KEY}:${SECRET_KEY} \
+     -H 'Content-Type: application/json' \
+     ${EMQX_DASHBOARD_BASE_URL}/api/v5/authorization/sources/built_in_database/rules/all \
+     --data '{
+               "rules": [
+                 {
+                   "action": "subscribe",
+                   "topic": "/user/${client_id}",
+                   "permission": "allow"
+                 },
+                 {
+                   "action": "subscribe",
+                   "topic": "/user/${username}",
+                   "permission": "allow"
+                 },
+                 {
+                   "action": "subscribe",
+                   "topic": "#",
+                   "permission": "deny"
+                 }
+               ]
+             }'
